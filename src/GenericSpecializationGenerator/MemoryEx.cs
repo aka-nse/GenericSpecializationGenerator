33c15ad4148495a6754b29f5c2df498fd3f99c34
aka-nse <nakase.d1220@gmail.com>
2025-11-30 14:24:47 +0900

    feature/migrate-net10 (#26)
    * adds global.json
    
    * migrates into C# 14.0

--- src/GenericSpecializationGenerator/MemoryEx.cs
+++ src/GenericSpecializationGenerator/MemoryEx.cs
@@ -1,4 +1,4 @@
-﻿namespace GenericSpecializationGenerator;
+namespace GenericSpecializationGenerator;
 
 internal static class MemoryEx
 {
@@ -6,6 +6,22 @@ internal static class MemoryEx
         this ReadOnlySpan<T> sequence,
         T value,
         IEqualityComparer<T> comparer)
+    {
+        for (var i = 0; i < sequence.Length; ++i)
+        {
+            if (comparer.Equals(sequence[i], value))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int IndexOf<T, TDerived>(
+        this ReadOnlySpan<TDerived> sequence,
+        T value,
+        IEqualityComparer<T> comparer)
+        where TDerived : T
     {
         for (var i = 0; i < sequence.Length; ++i)
         {
